8c5890536fea21f4374315c9b3fc588a958584c4
slice-mani <mani.c@sliceit.com>
2022-01-27 12:49:39 +0530

    feat(padder): added stringpadder feature

---
+++ src/main/java/stringpadder/StringPadderImpl.java
@@ -0,0 +1,48 @@
+package stringpadder;
+
+public class StringPadderImpl implements StringPadder {
+  public StringPadderImpl() {}
+
+  @Override
+  public String padLeft(String stringToPad, int totalLength) {
+    return padLeft(stringToPad, totalLength, ' ');
+  }
+
+  @Override
+  public String padLeft(String stringToPad, int totalLength, char paddingCharacter) {
+    return getStringToBeAdded(stringToPad, totalLength, paddingCharacter) + stringToPad;
+  }
+
+  @Override
+  public String padRight(String stringToPad, int totalLength) {
+    return padRight(stringToPad, totalLength, ' ');
+  }
+
+  @Override
+  public String padRight(String stringToPad, int totalLength, char paddingCharacter) {
+    return stringToPad + getStringToBeAdded(stringToPad, totalLength, paddingCharacter);
+  }
+
+  /*It will pad middle side of the given string if the length of the string is less than total length*/
+  @Override
+  public String padMiddle(String stringToPad, int totalLength) {
+    return padMiddle(stringToPad, totalLength, ' ');
+  }
+
+  @Override
+  public String padMiddle(String stringToPad, int totalLength, char paddingCharacter) {
+    String left = stringToPad.substring(0, stringToPad.length() / 2 + 1);
+    String right = stringToPad.substring(stringToPad.length() / 2 + 1);
+    String middle = getStringToBeAdded(stringToPad, totalLength, paddingCharacter);
+    return left + middle + right;
+  }
+
+  private String getStringToBeAdded(String stringToPad, int totalLength, char paddingCharacter) {
+    int quantity = totalLength - stringToPad.length();
+    if (quantity > 0) {
+      return Character.toString(paddingCharacter).repeat(quantity);
+    } else {
+      return "";
+    }
+  }
+}
